1b27e0ec08c092b4bba2cc999b2a522accc44da0
Elainaisyes <elijah.chesser@students.cvtech.edu>
2026-03-12 15:17:18 -0500

    t

--- server.js
+++ server.js
@@ -11,5 +11,10 @@ app.use(cors());
 const importantKey = 'hErShEy21$!';
 
 function getUsers () {
-    
+    const data = fs.readFileSync('users.json');
+    return JSON.parse(data);
+}
+
+function saveUsers(users) {
+  fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 }
